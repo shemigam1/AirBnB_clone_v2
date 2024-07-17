7084e92f70194635a0c3b71feca85e95abb54469
semilore <semilore@omotade-michaels.com>
2024-07-17 15:24:21 +0100

    mysql setup

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,8 +3,8 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 USE hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL on hbnb_dev_db TO 'hbnb_dev'@'localhost';
-GRANT USAGE on *.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT ALL on hbnb_dev_db TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT USAGE on *.* TO 'hbnb_dev'@'localhost'; 
 -- USE performance_schema;
 GRANT SELECT on performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
