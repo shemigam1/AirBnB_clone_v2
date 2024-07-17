5084425f1bb13b394050119cf94e3d6a12afcfdd
semilore <semilore@omotade-michaels.com>
2024-07-17 15:29:32 +0100

    mysql test setup

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,16 @@
 -- setup mysql server for project
 -- more comments
+-- create db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- use db
 USE hbnb_dev_db;
+-- create user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- grant user privileges
 GRANT ALL on hbnb_dev_db TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+-- grant privilege on another db
 GRANT USAGE on *.* TO 'hbnb_dev'@'localhost'; 
 -- USE performance_schema;
 GRANT SELECT on performance_schema.* TO 'hbnb_dev'@'localhost';
+-- flush privileges
 FLUSH PRIVILEGES;
---
+++ setup_mysql_test.sql
@@ -0,0 +1,16 @@
+-- setup mysql server for testing project
+-- more comments
+-- create db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- use db
+USE hbnb_test_db;
+-- create user
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- grant user privileges
+GRANT ALL on hbnb_test_db TO 'hbnb_test'@'localhost' WITH GRANT OPTION;
+-- grant user privilege on another db
+GRANT USAGE on *.* TO 'hbnb_test'@'localhost'; 
+-- USE performance_schema;
+GRANT SELECT on performance_schema.* TO 'hbnb_test'@'localhost';
+-- flush privileges
+FLUSH PRIVILEGES;
