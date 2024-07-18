f839e64499c45ad46a156a8b981c74afa6c4efb3
semilore <semilore@omotade-michaels.com>
2024-07-18 12:39:46 +0100

    update places, reviews

--- models/city.py
+++ models/city.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column,  String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -11,3 +12,4 @@ class City(BaseModel, Base):
     
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    places = relationship('Place', backref='cities', cascade='all, delete')
--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -11,3 +12,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
+    places = relationship('Place', backref='user', cascade='all, delete')
+    reviews = relationship('Review', backref='user', cascade='all, delete')
