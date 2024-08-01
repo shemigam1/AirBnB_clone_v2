bca59d71415aa878a2b4727795a34a883b6c2a6c
semilore <semilore@omotade-michaels.com>
2024-08-01 14:05:09 +0100

    add: type enforced path variables

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+"""
+module contains flask applications
+"""
+
+from flask import Flask
+# from markupsafe import escape
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+# host = "0.0.0.0"
+# port = 5000
+
+
+@app.route("/")
+def hello_world():
+    """returns text string"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hello_hbnb():
+    """returns HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def hello_c(text):
+    """returns varible text"""
+    new_text = text.replace("_", " ")
+    return "C {}".format(new_text)
+
+
+@app.route("/python/", defaults={"text": "is cool"})
+@app.route("/python/<text>")
+def hello_python(text):
+    """returns varible text"""
+    new_text = text.replace("_", " ")
+    return "Python {}".format(new_text)
+
+
+@app.route("/number/<int:n>")
+def hello_n(n):
+    """return if n is int"""
+    #if type(n) == int:
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    """runs app"""
+    # app.run(host, port)
+    app.run(host='0.0.0.0', port=5000)
+    # app.run(debug=True)
